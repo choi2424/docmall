938b15f8a9ac3a7174d31bad9fe770d968be2836
choi2424 <choi242002@gmail.com>
2023-10-25 17:33:36 +0900

    회원가입 메일 인증 작업

--- src/main/java/com/docmall/controller/MemberController.java
+++ src/main/java/com/docmall/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.docmall.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.docmall.service.MemberService;
+import com.sun.org.apache.bcel.internal.generic.ISUB;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -25,9 +27,11 @@ public class MemberController {
 		
 		log.info("called... join");
 	}
-	
-	//비동기방식. ajax문법으로 호출
-	
+	// 비동기방식. ajax문법으로 호출
+	// 아이디 중복체크
+	// ResponseEntity클래스 ? httpentity를 상속받는, 결과 데이터와 HTTP 상태 코드를 직접 제어할 수 있는 클래스이다.
+	// 3가지 구성요소 HttpStatus , HttpHeaders , HttpBody
+	// ajax기능과 함께 사용
 	@GetMapping("/idCheck")
 	public ResponseEntity<String> idCheck(String mbsp_id){
 		
@@ -36,6 +40,14 @@ public class MemberController {
 		ResponseEntity<String>  entity = null;
 		
 		// 서비스 메서드 호출구문 작업.
+		String idUse = "";
+		if(memberService.idCheck(mbsp_id) != null) {
+			idUse = "no"; // 아이디가 존재하여 , 사용 불가능
+		}else {
+			idUse = "yes"; // 아이디가 존재하지 않아 , 사용 가능
+		}
+		
+		entity = new ResponseEntity<String>(idUse , HttpStatus.OK);
 		
 		return entity;
 	}
